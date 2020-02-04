7887c069ac5dd9e0c6f72d6a07b3b44f2fd02a9f
DarriusAlexander <darriush@mail.usf.edu>
2020-02-04 02:04:18 -0500

    .

--- rootfs/download-extra.sh
+++ rootfs/download-extra.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 curl -LO https://github.com/DarriusAlexander/stayncharge-content/archive/master.zip  | grep -oP '"tag_name": "\K(.*)(?=")'
-unzip master.zip -d /opt/bitnami/moodle
+unzip master.zip -d /opt/bitnami
 mv /opt/bitnami/stayncharge-content-master /opt/bitnami/moodle
